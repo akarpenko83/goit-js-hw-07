b76e9ee4f95d058aaeaebaae336350c83c2583cf
Anton K <akarpenko83@gmail.com>
2023-03-15 17:57:24 +0200

    markup done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,6 +1,32 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryContainer = document.querySelector(".gallery")
 
-console.log(galleryItems);
 
-// test
+galleryContainer.insertAdjacentHTML("beforeend", createGalleryMarkup(galleryItems));
+
+galleryContainer.addEventListener('clic', onPictureClick)
+
+function onPictureClick(evt) {
+    evt.preventDefault();
+    console.log(evt.currentTarget);
+};
+
+function createGalleryMarkup(galleryItems) {
+    return galleryItems.map(({description, original, preview}) => {
+        return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+        </div>
+        `;
+    })
+    .join("");
+ };
+
